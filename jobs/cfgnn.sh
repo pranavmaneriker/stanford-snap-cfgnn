8e88c608b2715c462e1a8554b23813928aa47db1
Aditya Vadlamani <aditya.t.vadlamani@gmail.com>
2023-11-13 12:17:07 -0500

    Added grid search script

---
+++ jobs/cfgnn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account PAS2030
+#SBATCH --partition=gpuserial
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=28
+#SBATCH -J conformal_5_1
+#SBATCH --gres=gpu:1
+#SBATCH --time=1-00:00:00
+#SBATCH -o /users/PAS2065/adityatv/jobs/slurm-out-%A.txt
+#SBATCH -e /users/PAS2065/adityatv/jobs/slurm-err-%A.txt
+
+. /users/PAS2065/adityatv/miniconda3/etc/profile.d/conda.sh
+conda deactivate
+conda activate fairgraph
+
+# __script_start__
+cd $HOME/stanford-snap-cfgnn
+srun python train.py \
+    --model GCN \
+    --dataset $1 \
+    --device cuda \
+    --alpha $2 \
+    --use_fixed_aps
---
+++ jobs/grid_search.sh
@@ -0,0 +1,7 @@
+for dataset in PubMed_CF
+do
+    for alpha in 0.05 0.1 0.15 0.2 0.25 0.3
+    do
+        sbatch cfgnn.sh $dataset $alpha
+    done
+done
